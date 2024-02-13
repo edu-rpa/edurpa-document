698998de186dd42eb582ec0683b1b3d1c15cd9bb
David Huynh <78911364+02david20@users.noreply.github.com>
2024-02-13 20:14:20 +0700

    Upload package struture

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="EduRPA",
-    version="0.0.1",
+    version="0.0.2",
     author="david",
     author_email="davidhuynh0222@gmail.com",
     description="education librabry for RPA",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/edu-rpa/edu-rpa-library",
-    packages=setuptools.find_packages(),
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
     classifiers=[
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
